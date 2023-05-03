4a5304b8f71b881e677d5bd1361ebc2c8ceaf085
Anatolii Radionov <anatolii.radionov2002@gmail.com>
2023-05-03 20:47:32 +0200

    AABB surrounding_box function bug fix

--- src/aabb.cpp
+++ src/aabb.cpp
@@ -13,9 +13,9 @@ AABB AABB::surrounding_box(const AABB& box0, const AABB& box1)
         fmin(box0.minimum[2], box1.minimum[2]) };
 
     vec::vec3 max{
-        fmin(box0.maximum[0], box1.maximum[0]),
-        fmin(box0.maximum[1], box1.maximum[1]),
-        fmin(box0.maximum[2], box1.maximum[2]) };
+        fmax(box0.maximum[0], box1.maximum[0]),
+        fmax(box0.maximum[1], box1.maximum[1]),
+        fmax(box0.maximum[2], box1.maximum[2]) };
 
     return AABB(min, max);
 }
